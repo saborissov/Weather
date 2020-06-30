8c6566f4563daf82956bb4e5428cee7a8b278e1e
17586317 <Borisov.S.Alek@sberbank.ru>
2020-06-30 22:21:58 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 17586317 on 30.06.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
